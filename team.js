7977172ae45c8ba569e104509bc5fab912b6b99c
LuisGrandini <grandiniluis18@gmail.com>
2024-01-18 13:07:39 +0100

    Datos Luis

--- team.js
+++ team.js
@@ -1,1 +1,14 @@
-const student = [];
+const student = [
+    {
+        name : "Luis",
+        surname : "Grandini",
+        age : "26",
+        city : "Caracas",
+        hobby : "Videogames",
+        favoriteFood : "Pizza",
+        favoriteVideoGame : "Fire emblem Three houses",
+        favoriteFilm : "La gran apuesta",
+        favoriteBook : "El invierno del mundo",
+        petName : ""
+    }
+];
