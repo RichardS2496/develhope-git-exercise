2fce69bf1e79560b04ba4c7cada232f21e99f9b4
pedrucchio <pedrograndini@gmail.com>
2024-01-18 10:46:15 +0100

    Info Pedro

--- team.js
+++ team.js
@@ -1,1 +1,37 @@
-const student = [];
+const student = [
+    {
+        name:"Pedro",
+        surname:"Grandini",
+        age:28,
+        city:"Madrid",
+        hobby:"Videojuegos",
+        favoritefood:"Pasta",
+        favoritevideogame:"The Legend of Heroes",
+        favoritefilm:"Lord of the Rings",
+        favoritebook:"Harry Potter",
+        petname:""
+    }
+];
+
+
+/*
+name
+surname
+age
+city
+hobby
+favoritefood
+favoritevideogame
+favoritefilm
+favoritebook
+petname
+*/
+
+/*
+- Print the team in alphabetical order (surname name).
+- Print the team in age order (name age).
+- Print the team middle age.
+- Print who has a pet (name petName).
+- Print who wrote ‘LOL’ or ‘League Of Legends’ as a favorite video game. (name).
+- Print if there are some members with the same name (name).
+*/
